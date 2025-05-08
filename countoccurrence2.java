fa87420880096c68e908e68b538f39caf44fa8c6
Sneha Pandey <sneha.cs231210@global.org.in>
2025-05-08 20:59:12 +0530

    hello

---
+++ countoccurrence2.java
@@ -0,0 +1,16 @@
+//Count the number of occurrences of an element
+class countoccurrence2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 37, 4, 5, 37, 2, 11};
+        int countelement = 37;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == countelement) {
+                count++;
+            }
+        }
+
+        System.out.println("Element " + countelement + " occurs " + count + " times.");
+    }
+}
